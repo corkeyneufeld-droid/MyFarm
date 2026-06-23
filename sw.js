9db9fb91ad73f68d58fab0ff2928f593b3856a23
corkeyneufeld-droid <corkeyneufeld@gmail.com>
2026-06-22 20:55:00 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE = 'myfarm-v2';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
